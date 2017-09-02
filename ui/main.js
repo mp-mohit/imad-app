033f3f5d93e6fc5fdc042c0f0f31426138faa48f
mp-mohit <mp-mohit@users.noreply.github.com>
2017-09-02 16:30:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,63 @@
-console.log('Loaded!');
+//Counter Code
+var button = document.getElementById('counter');
+button.onclick = function()
+{
+    //Create a request Object
+    var request = new XMLHttpRequest();
+    //Capture the response and store it in the variable
+    request.onreadystatechange = function()
+    {
+      if(request.readyState === XMLHttpRequest.DONE)
+      {
+        //Take some action  
+        if(request.status === 200)
+        {
+         var counter = request.responseText;
+         var span = document.getElementById('count');
+         span.innerHTML = counter.toString();
+        }
+      }
+      //Not done yet then don't do anything
+    };
+    //Make the request
+    request.open('GET','http://pneel813.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
+//Submit name
+
+var Submit = document.getElementById('Submit_btn');
+Submit.onclick = function()
+{
+   var request = new XMLHttpRequest();
+    //Capture the response and store it in the variable
+    request.onreadystatechange = function()
+    {
+      if(request.readyState === XMLHttpRequest.DONE)
+      {
+        //Take some action  
+        if(request.status === 200)
+        {
+         var names = request.responseText;
+         names = JSON.parse(names);
+         var list = '';
+         for(i=0;i<names.length;i++)
+         {
+             list += '<li>' + names[i] + '</li>';
+         }
+         var Ul = document.getElementById('namelist');
+         Ul.innerHTML = list;
+        }
+      }
+      //Not done yet then don't do anything
+    };
+    //Make the request
+    var nameInput = document.getElementById('name'); 
+    var n1 = nameInput.value;
+    request.open('GET','http://pneel813.imad.hasura-app.io/submit-name?name=' + n1, true);
+    request.send(null);
+
+  //Make a request to the server and send a name
+ //Capture a list  of names and render it as a list
+ 
+ 
+};
